4a889538ce0f12285ce052c1d524e451b195c804
Ghulam Murtaza <ghulammurtazagopang3@gmail.com>
2023-10-04 16:14:15 +0500

    done with get todos api

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,6 +1,6 @@
 const Todo = require('../models/todo');
 
-// Create
+// Create To-do api
 exports.createTodo = async (req, res) => {
     try {
 const todo = new Todo(req.body);
@@ -9,5 +9,15 @@ res.json(savedTodo);
     } catch (error){
         res.status(500).json({error: "Could not create the Todo" });
         console.log(error.message);
+    }
+}
+
+// Get all todos 
+exports.getAllTodos = async (req, res) => {
+    try {
+const todos = await Todo.find();
+res.json(todos);
+    }catch (error){
+        res.status(500).json({error: "Could not retrieve todos."});
     }
 }
--- routes/todo.js
+++ routes/todo.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const todoController = require('../controllers/todo');
 
 
-//create
+//create Todo
 router.post('/api/todos', todoController.createTodo);
+
+//Get Todos
+router.get('/api/getTodos', todoController.getAllTodos);
 
 
 
